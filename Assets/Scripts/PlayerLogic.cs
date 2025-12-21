aed3bf9b91b4ddefad55594f674ad3aa6fab2c9e
PlazmaDolphin <plasmadolphin0@gmail.com>
2025-12-21 18:27:58 -0500

    player flips to direction he's moving

--- Assets/Scripts/PlayerLogic.cs
+++ Assets/Scripts/PlayerLogic.cs
@@ -6,6 +6,7 @@ public class PlayerMove : MonoBehaviour
     public Camera cam;
     public Collider2D playerCollider;
     public TextMeshProUGUI scrapText; // TODO: Replace with proper UI element
+    public SpriteRenderer playerSprite;
     private float speed = 5f;
     private int scrapCollected = 0;
 
@@ -20,9 +21,16 @@ public class PlayerMove : MonoBehaviour
     {
         // Get WASD input and normalize
         Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        // flip sprite based on input
+        if (input.x < 0)
+            playerSprite.flipX = true;
+        else if (input.x > 0)
+            playerSprite.flipX = false;
         input.Normalize();
         // Move the player based on input
         transform.Translate(input * speed * Time.deltaTime, Space.World);
+        // force player rotation to be 0
+        transform.rotation = Quaternion.Euler(0, 0, 0);
         // center camera to player position
         cam.transform.position = new Vector3(transform.position.x, transform.position.y, cam.transform.position.z);
         scrapText.text = "Scrap: " + scrapCollected;
